1ade5241c22d463250056c287e80990d61f62d7e
Bhavy Savaliya <154491385+bhavysavaliya@users.noreply.github.com>
2025-04-04 14:57:49 +0530

    Create practical_10.java

---
+++ practical_10.java
@@ -0,0 +1,16 @@
+class Bhavy {
+ 
+  final int MAX_VALUE = 100;
+
+  final void displayMessage() {
+      System.out.println("This is a final method.");
+  }
+
+  public static void main(String[] args) {
+      Bhavy s1 = new Bhavy();
+
+      System.out.println("MAX_VALUE: " + s1.MAX_VALUE);
+
+      s1.displayMessage();
+  }
+}
